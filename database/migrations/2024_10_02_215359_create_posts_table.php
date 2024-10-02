ad0bc8b2f4273a0b21714d788895d5d5c4000a08
vilomy <vilomy2@gmail.com>
2024-10-03 01:18:20 +0300

    создал миграции

---
+++ database/migrations/2024_10_02_215359_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedBigInteger('categories_id')->nullable();
+            $table->timestamps();
+
+            $table->index('categories_id', 'post_category_idx');
+            $table->foreign('categories_id', 'post_category_fk')->on('categories')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
